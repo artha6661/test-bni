a2912c8675fd26449fe3544f94a8de6a9cb90bbb
artha6661 <53061565+artha6661@users.noreply.github.com>
2023-10-28 14:26:32 +0700

    First Merge

---
+++ src/main/java/com/testbni/artha/ArthaBastantaApplication.java
@@ -0,0 +1,13 @@
+package com.testbni.artha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthaBastantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArthaBastantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testbni/artha/ArthaBastantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testbni.artha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthaBastantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
